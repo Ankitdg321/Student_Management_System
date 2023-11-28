03ecdd2180a3bdcabfd72693b588639ff6077547
ANKIT DILIP GAIKKWAD <143960237+Ankitdg321@users.noreply.github.com>
2023-11-28 23:32:09 +0530

    Create student.java

---
+++ student.java
@@ -0,0 +1,10 @@
+package modal;// must look
+
+public class student {
+
+	public String name;
+	public int studentID;
+	public String address;
+	public float cgpa;
+
+}//here i just created  CUSTUM DATATYPE  which can hold many dtattypes within it.
